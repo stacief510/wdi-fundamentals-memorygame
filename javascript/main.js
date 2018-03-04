748ff20f60bbe329dcd34888cd485fce6010798e
stacief510 <staciemfraser@gmail.com>
2018-03-03 17:02:10 -0800

    created game logic2

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,47 @@
-console.log("Up and running!");
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log ("Up and running!");
+
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage:"images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+},
+];
+
+var cardsInPlay = [];
+
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
+	}
+	};
+
+var flipCard = function(cardId) {
+	console.log("Player flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	checkForMatch();
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+ };
+
+flipCard(0);
+flipCard(2);
