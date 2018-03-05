7975e0709c4ebafa818f61fc8c5372c34cafb4a0
stacief510 <staciemfraser@gmail.com>
2018-03-04 16:47:18 -0800

    created game logic3

--- javascript/main.js
+++ javascript/main.js
@@ -4,44 +4,75 @@ var cards = [
 {
 	rank: "queen",
 	suit: "hearts",
-	cardImage:"images/queen-of-hearts.png"
+	cardImage:"../images/queen-of-hearts.png",
 },
 {
 	rank: "queen",
 	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
+	cardImage: "../images/queen-of-diamonds.png",
 },
 {
 	rank: "king",
 	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
+	cardImage: "../images/king-of-hearts.png",
 },
 {
 	rank: "king",
 	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
+	cardImage: "../images/king-of-diamonds.png"
 },
 ];
 
 var cardsInPlay = [];
 
+var userScore = 0;
+console.log(userScore);
+
 var checkForMatch = function(){
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
+			//userScore += 1;
+			//var score = getElementById('score').innerHTML(userScore);
 		} else {
 			alert("Sorry, try again.");
 		}
 	}
 	};
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("Player flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	
+	this.setAttribute('src', cards[cardId].cardImage);
+		checkForMatch();
  };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "../images/back.png");
+		cardElement.setAttribute('data-id', i);
+
+		cardElement.addEventListener('click', flipCard);
+
+		document.getElementById('game-board').appendChild(cardElement);
+}
+};
+
+var resetBoard = function(){
+for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', '../images/back.png');
+	cardElement.setAttribute('data-id', i);
+
+	var resetButton = document.getElementsByTagName('button');
+	resetButton.addEventListener('click', resetBoard);
+}
+};
+
+
+createBoard();
